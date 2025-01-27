00334ebcad278b9f9ee10eb3b24bb5a84bbf5cd7
Falco24 <gamerfalco24@gmail.com>
2025-01-27 14:28:14 +0100

    billentyuzet

--- MasodikFelev/Valami.cs
+++ MasodikFelev/Valami.cs
@@ -5,27 +5,50 @@ class Valami{
 
     const int M = 30;
 
+    static int[,] palya = new int[N,M];
 
+    static void KajaElhelyez(int db){
+        Random rnd = new ();
+        for (int i = 0; i < db; i++)
+        {
+            int kajaX = rnd.Next(0,N);
+            int kajaY = rnd.Next(0,M);
+            int kajaErtek = rnd.Next(5, 10);
+            palya[kajaX, kajaY] = kajaErtek;
+        }
+    }
 
-
-
-
+    static void KajaRajz(){
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < M; y++)
+            {
+                if ( palya[x,y] > 0 ){
+                    Console.SetCursorPosition(x,y);
+                    Console.Write(palya[x,y]);
+                }
+            }
+        }
+    }
 
     static void Main(){
         Console.SetWindowSize(N, M);
 
         bool exit = false;
-        int timer = 100;
+        int timer = 0;
         int babuX = N/2;
         int babuY = M/2;
         Console.SetCursorPosition(babuX, babuY);
         Console.Write('X');
-
+        KajaElhelyez(30);
         while(!exit){
-            timer--;
-            Thread.Sleep(100);
+            timer++;
+
             Console.SetCursorPosition(0,0);
             Console.Write(timer);
+            KajaRajz();
+
+            Thread.Sleep(100);
             if ( Console.KeyAvailable ){
                     ConsoleKeyInfo pressed = Console.ReadKey(true); 
                     if (pressed.Key == ConsoleKey.Escape){
