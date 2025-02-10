3a64efae07b226f39c09f54c9283f74eb360b472
Falco24 <gamerfalco24@gmail.com>
2025-02-10 14:57:15 +0100

    jobbbal

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class BalJobbSzam{
+
+        const int length = 80;
+        static List<int> fromLeft = new List<int>();
+        static List<int> fromRight = [];
+
+        static void NewNumber( double prob ) {
+            Random rnd = new ();
+
+            if ( rnd.NextDouble() < prob ) {
+
+            }
+        }
+
+
+
+
+
+    public void Main() {
+        Console.SetWindowSize(85, 10);
+    }
+}
