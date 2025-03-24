76878aa8593a9a79b8a4566b3b390310b855b8ea
Falco24 <gamerfalco24@gmail.com>
2025-03-24 10:38:20 +0100

    Rendezesek - buborek

--- MasodikFelev/oraics.cs
+++ MasodikFelev/oraics.cs
@@ -1,4 +1,10 @@
 using System;
+
+
+struct Par{
+    public int ossze, csere;
+}
+
 class Orai{
        static int[] tomb;
        const int N = 20;
@@ -14,14 +20,41 @@ class Orai{
         tomb[i] = tomb[j];
         tomb[j] = plusz;
     }
-    static void buborek(){
+    static Par minKivRend(){
+        Par par;
+        par.ossze = 0;
+        par.csere = 0;
+
+        for ( int honnan = 0; honnan <= N-2; honnan++){
+            int minind = honnan;
+            for ( int i = honnan + 1; i < N; i++ )
+            {
+                par.ossze++;
+                if ( tomb[i] < tomb[minind] )
+                {
+                    minind = i;
+                }
+            }
+            par.csere++;
+            csere(honnan, minind);
+        }
+
+        return par;
+    }
+    static Par buborek(){
+        Par par;
+        par.ossze = 0;
+        par.csere = 0;
         for ( int meddig = N-2; meddig >= 0; meddig--){
             for (int mutato = 0; mutato <= meddig; mutato++){
+                par.ossze++;
                 if ( tomb[mutato] > tomb[mutato+1]){
+                    par.csere++;
                     csere(mutato, mutato+1);
                 }
             }
         }
+        return par;
     }
     static void Main(){
         tomb = new int[N];
@@ -29,8 +62,16 @@ class Orai{
         for (int i = 0; i < N; i++){
             tomb[i] = rnd.Next(10,100);
         }
-        kiir();
-        boburek();
-        kiir();
+        // kiir();
+
+        int[] ment = (int[]) tomb.Clone();
+
+        Par bub = boburek();
+        // kiir();
+        System.Console.WriteLine(bub.ossze+"--"+bub.csere);
+        tomb = (int[]) ment.Clone();
+        Par min = minKivRend;
+        // kiir();
+        System.Console.WriteLine(min.ossze+"--"+min.csere);
     }
 }
